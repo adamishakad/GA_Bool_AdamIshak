afd14d8846a5bea7eaaf5a7d456a7992ab488f45
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-02-02 23:47:31 -0800

    updated Program File

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace GA_Bool_AdamIshak
+﻿// Adam Ishak
+
+using System.ComponentModel.Design;
+
+namespace GA_Bool_AdamIshak
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool isSunny = true;
+            bool isWarm = false;
+
+            if(isSunny && isWarm)
+            {
+                Console.WriteLine("It's a sunny and warm day!");
+            }else if(isSunny)
+            {
+                Console.WriteLine("It's sunny but not warm!");
+            }else
+            {
+                Console.WriteLine("It is not sunny today");
+            }
+            bool isWeekend = true;
+            int dayOfWeek = 6;
+            bool isRestDay = isWeekend || (dayOfWeek == 6);
+            {
+                Console.WriteLine("It's a rest day!");
+            }
+            
+            {
+                Console.WriteLine("It's a regular day.");
+            }
+            
+
         }
+       
     }
 }
